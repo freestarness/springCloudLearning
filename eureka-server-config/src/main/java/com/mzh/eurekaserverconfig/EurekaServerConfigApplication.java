1c32e3d20fe503c72cc8ca6901d3694b217c03f2
zh-ma <670759275@qq.com>
2018-06-27 17:29:46 +0800

    分布式config改造 增加config的eureka注册中心

---
+++ eureka-server-config/src/main/java/com/mzh/eurekaserverconfig/EurekaServerConfigApplication.java
@@ -0,0 +1,14 @@
+package com.mzh.eurekaserverconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerConfigApplication.class, args);
+    }
+}
